7e5c7b202ab001896ba28737bff86376415962c5
Asadullah <asadullahabbasi225@gmail.com>
2025-12-01 20:11:38 +0500

    email verification url issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://rideshare.com',
+            url: 'https://carpool-world-git-main-asadullahabbasis-projects.vercel.app',
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 1,
         },
         {
-            url: 'https://rideshare.com/auth',
+            url: 'https://carpool-world-git-main-asadullahabbasis-projects.vercel.app/auth',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://rideshare.com/dashboard',
+            url: 'https://carpool-world-git-main-asadullahabbasis-projects.vercel.app/dashboard',
             lastModified: new Date(),
             changeFrequency: 'always',
             priority: 0.5,
